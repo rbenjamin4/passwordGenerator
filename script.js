31037a468129f4d9439a8be6da92d60a43fd347b
Reed Benjamin <reed@Reeds-MacBook-Pro.local>
2023-12-08 13:50:24 -0500

    Added pseudocode to breakdown steps

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log('Button is working')
+
+// 1. Prompt user for length of password (b/w 8 and 128)
+// 2. Prompt user for use of uppercase, lowercase, numbers, special characters
+
+
+
+// 3. Validate input (at least one character type selected)
+
+
+// 4. Generate password based on selected criteria
+
+
+// 5. Display password on the page
+  return('Generated password')
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
