119774237fa670e91d96197b36fece6f82637860
Reed Benjamin <reed@Reeds-MacBook-Pro.local>
2023-12-08 14:35:36 -0500

    Created password length prompt and confirmations for each character type

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 function generatePassword() {
   console.log('Button is working')
 
 // 1. Prompt user for length of password (b/w 8 and 128)
+let passwordLength = prompt('Please choose a password length between 8 and 128 characters.')
+console.log(passwordLength)
+
 // 2. Prompt user for use of uppercase, lowercase, numbers, special characters
+let charLowerCase = confirm('Would you like to include lowercase letters?')
+console.log(charLowerCase)
 
+let charUpperCase = confirm('Would you like to include uppercase letters?')
+console.log(charUpperCase)
 
+let charNumbers = confirm('Would you like to include numbers?')
+console.log(charNumbers)
+
+let charSpecial = confirm('Would you like to include special characters?')
+console.log(charSpecial)
 
 // 3. Validate input (at least one character type selected)
 
