f95f5e24d613fca4a5400bc17f940f988afad196
Reed Benjamin <reed@reeds-mbp.lan>
2023-12-10 14:31:38 -0500

    Removed console logs for testing and formatted code

--- script.js
+++ script.js
@@ -1,66 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
+// Begin password 
 function generatePassword() {
-  console.log('Button is working')
 
-// 1. Prompt user for length of password (b/w 8 and 128)
-let passwordLength = prompt('Please choose a password length between 8 and 128 characters.')
-console.log(passwordLength)
-// Validate input
-if (passwordLength < 8 || passwordLength > 128 || NaN) {
-  alert('Your password length is outside the required range of 8 and 128 characters.')
-  return;
-}
+  // Prompt user for length of password (b/w 8 and 128)
+  let passwordLength = prompt('Please choose a password length between 8 and 128 characters.')
 
-// 2. Prompt user for use of uppercase, lowercase, numbers, special characters
-let charLowerCase = confirm('Would you like to include lowercase letters?')
-console.log(charLowerCase)
+  // Validate input
+  if (passwordLength < 8 || passwordLength > 128 || NaN) {
+    alert('Your password length is outside the required range of 8 and 128 characters.')
+    return;
+  }
 
-let charUpperCase = confirm('Would you like to include uppercase letters?')
-console.log(charUpperCase)
+  // Prompt user for use of uppercase, lowercase, numbers, special characters
+  let charLowerCase = confirm('Would you like to include lowercase letters?')
 
-let charNumbers = confirm('Would you like to include numbers?')
-console.log(charNumbers)
+  let charUpperCase = confirm('Would you like to include uppercase letters?')
 
-let charSpecial = confirm('Would you like to include special characters?')
-console.log(charSpecial)
+  let charNumbers = confirm('Would you like to include numbers?')
 
-if (!charLowerCase && !charUpperCase && !charNumbers && !charSpecial){
-  alert('Please select at least one character type')
-}
+  let charSpecial = confirm('Would you like to include special characters?')
 
-// 3. Validate input (at least one character type selected)
+  // Validate input (at least one character type selected)
+  if (!charLowerCase && !charUpperCase && !charNumbers && !charSpecial) {
+    alert('Please select at least one character type')
+  }
 
-let selectedCriteria = ''
-let password = ''
+  // Provide character options
+  let selectedCriteria = ''
+  let password = ''
 
-if (charLowerCase){
-  selectedCriteria += 'abcdefghijklmnopqrstuvwxyz'
-}
-if (charUpperCase){
-  selectedCriteria += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-}
-if(charNumbers){
-  selectedCriteria += '1234567890'
-}
-if(charSpecial){
-  selectedCriteria += ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
-}
-console.log(selectedCriteria)
+  if (charLowerCase) {
+    selectedCriteria += 'abcdefghijklmnopqrstuvwxyz'
+  }
+  if (charUpperCase) {
+    selectedCriteria += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+  if (charNumbers) {
+    selectedCriteria += '1234567890'
+  }
+  if (charSpecial) {
+    selectedCriteria += ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
+  }
 
+  // Generate password based on selected criteria
+  for (let i = 0; i < passwordLength; i++) {
+    password += selectedCriteria[Math.floor(Math.random() * selectedCriteria.length)]
+  }
 
-// 4. Generate password based on selected criteria
-for (let i=0; i < passwordLength; i++) {
-  password += selectedCriteria[Math.floor(Math.random() * selectedCriteria.length)]
-}
-console.log(password)
-
-
-// 5. Display password on the page
-  return(password)
+  // Display password on the page
+  return (password)
 }
 
 // Write password to the #password input
@@ -69,7 +59,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
